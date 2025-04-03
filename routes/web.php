edfae8f894a6a184582b8c299fa4e79890a904a8
Erickpe8 <ericksperezc@gmail.com>
2025-04-02 21:18:03 -0500

    Documentación en el web.php

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // Ruta de la página de inicio
 });
 
+// Ruta para el panel de control, accesible solo por usuarios autenticados y verificados
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Grupo de rutas protegidas por autenticación
 Route::middleware('auth')->group(function () {
+    // Ruta para editar el perfil del usuario
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    
+    // Ruta para actualizar el perfil del usuario
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    
+    // Ruta para eliminar el perfil del usuario
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Se incluye el archivo de rutas de autenticación generadas por Laravel Breeze, Jetstream u otro paquete de autenticación
 require __DIR__.'/auth.php';
 
-
+// Grupo de rutas RESTful para la gestión de productos
 Route::resource('productos', ProductoController::class);
