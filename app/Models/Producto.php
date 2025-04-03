cf08b6ff628d10991224dc8603bb12c34dbc5c76
Erickpe8 <ericksperezc@gmail.com>
2025-04-02 21:47:41 -0500

    Se creo el Factory y el Seeder

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 /**
  * Clase Producto
@@ -23,6 +25,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Producto extends Model
 {
+    use HasFactory;
     /**
      * Número de elementos por página para la paginación.
      *
--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -9,12 +9,12 @@ class ProductoFactory extends Factory
 {
     protected $model = Producto::class;
 
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => $this->faker->word(),
             'descripcion' => $this->faker->sentence(),
-            'precio' => $this->faker->randomFloat(2, 10, 500),
+            'precio' => $this->faker->numberBetween(1, 100) * 1000,
             'stock' => $this->faker->numberBetween(1, 100),
         ];
     }
